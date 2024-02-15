d19b92b4b28b746b687abef032813dc90fa8bbec
dev05 <dev05@dev.co-mit.com>
2024-02-15 10:42:24 +0700

    list post

--- php/src/connection.php
+++ php/src/connection.php
@@ -6,7 +6,7 @@ class DB
     public static function getInstance() {
         if (!isset(self::$instance)) {
           try {
-            self::$instance = new PDO('mysql:host=localhost:8083;dbname=MYSQL_DATABASE', 'MYSQL_USER', 'MYSQL_PASSWORD');
+            self::$instance = new PDO('mysql:host=db:3306;dbname=MYSQL_DATABASE', 'MYSQL_USER', 'MYSQL_PASSWORD');
             self::$instance->exec("SET NAMES 'utf8'");
           } catch (PDOException $ex) {
             die($ex->getMessage());
